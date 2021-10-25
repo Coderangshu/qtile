a79643a9be878f1efd4b0adac695c82fdfb16b01
Angshuman Sengupta <senguptaangshuman17@gmail.com>
2021-10-26 01:23:04 +0530

    all confs added

--- autostart.sh
+++ autostart.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-dunst &
-picom &
+#dunst &
+#picom &
 nm-applet &
 feh --randomize --bg-fill -D 2 ~/Wallpapers/* &
-noisetorch -i alsa_input.pci-0000_05_00.6.analog-stereo &
-nvidia-settings --assign CurrentMetaMode="nvidia-auto-select +0+0 { ForceFullCompositionPipeline = On }"
+#noisetorch -i alsa_input.pci-0000_05_00.6.analog-stereo &
+#nvidia-settings --assign CurrentMetaMode="nvidia-auto-select +0+0 { ForceFullCompositionPipeline = On }"
